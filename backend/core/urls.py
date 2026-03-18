5792f44e5c13df0dbdb735f768c60c95a752cef9
lara bautista <lara.bautista16@gmail.com>
2026-03-18 17:04:04 +0100

    Add URL path for sitters search

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,8 +1,12 @@
 from rest_framework.routers import DefaultRouter
-from .views import SitterViewSet, AvailableSlotViewSet
+from django.urls import path
+from .views import SitterViewSet, AvailableSlotViewSet, SitterSearchView
+
 
 router = DefaultRouter()
 router.register(r"sitters", SitterViewSet)
 router.register(r"slots", AvailableSlotViewSet)
 
-urlpatterns = router.urls
+urlpatterns = [
+    path("sitters/search/", SitterSearchView.as_view()),
+] + router.urls
