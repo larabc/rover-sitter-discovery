f130c299a5fc369eec6914ca25b6339ec51e21ce
lara bautista <lara.bautista16@gmail.com>
2026-03-24 15:04:54 +0100

    Add navigation between home and sitter or owner main views

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,10 +1,25 @@
-import { View, Text } from 'react-native'
+import { View, Text, Button } from 'react-native'
 import React from 'react'
+import { useRouter } from 'expo-router';
+
 
 export default function App() {
+    const router = useRouter();
+
+
     return (
         <View>
-            <Text>App</Text>
+            <Text>You are a dog...</Text>
+            <Button
+                onPress={() => router.push('/(sitter)/availability')}
+                title="Go to Sitter Role"
+                accessibilityLabel="Button that goes to sitter availaibilty page"
+            />
+            <Button
+                onPress={() => router.push('/(owner)/search')}
+                title="Go to Owner Role"
+                accessibilityLabel="Button that goes to owner role search page"
+            />
         </View>
     )
 }
