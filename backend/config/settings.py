c66a37620997345d9453eda94f3b204ba67e176e
lara bautista <lara.bautista16@gmail.com>
2026-03-25 12:26:39 +0100

    hotfix: fix typo in middleware

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -49,7 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    "corseheaders.middleware.CorsMiddleWare",
+    "corsheaders.middleware.CorsMiddleWare",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
