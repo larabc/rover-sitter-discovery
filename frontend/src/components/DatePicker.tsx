a000f36c3a27fb756111c23e89cb65e46f35d3b1
lara bautista <lara.bautista16@gmail.com>
2026-03-31 16:13:21 +0200

    Add datepicker component for date selector in owner search

---
+++ frontend/src/components/DatePicker.tsx
@@ -0,0 +1,55 @@
+
+import React from 'react'
+import { dateToTimeString } from '../utils/timeUtils'
+import DateTimePicker, { DateTimePickerAndroid } from '@react-native-community/datetimepicker'
+import { Platform, Pressable, Text, StyleSheet } from 'react-native'
+import { colors, spacing } from '../constants/theme'
+
+
+interface DatePickerProps {
+    value: Date,
+    onChange: (selectedDate: Date) => void,
+}
+
+export default function DatePicker({ value, onChange }: DatePickerProps) {
+    if (Platform.OS === 'android') {
+        return (
+            <Pressable style={styles.hourInput} onPress={() => {
+                DateTimePickerAndroid.open({
+                    value: value,
+                    mode: 'date',
+                    onChange: (event, selectedDate) => {
+                        if (event.type === 'set' && selectedDate) {
+                            onChange(selectedDate)
+                        }
+                    }
+                })
+            }}>
+                <Text>{dateToTimeString(value)}</Text>
+            </Pressable>
+        )
+    }
+
+    return (
+        <DateTimePicker
+            value={value}
+            mode="date"
+            display='compact'
+            onChange={(event, selectedDate) => {
+                if (event.type === 'set' && selectedDate) {
+                    onChange(selectedDate)
+                }
+            }}
+        />
+    )
+}
+
+const styles = StyleSheet.create({
+    hourInput: {
+        padding: spacing.md,
+        borderWidth: 2,
+        borderColor: colors.border,
+        borderRadius: 4,
+        flex: 1,
+    }
+});
--- frontend/src/utils/timeUtils.ts
+++ frontend/src/utils/timeUtils.ts
@@ -10,3 +10,9 @@ export const timeStringToDate = (timeString: string): Date => {
 export const hoursToTimeString = (hours: number): string => {
     return hours.toString().padStart(2, '0') + ':00:00'
 }
+
+export const dateToTimeString = (date: Date): string => {
+    return date.getHours().toString().padStart(2, '0') + ':00:00'
+}
+
+//TODO: unify this similar functions
