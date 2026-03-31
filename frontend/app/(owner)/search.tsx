f1d5cb95f222d823c5ea0b1ec1e8130b5b9aa8e0
lara bautista <lara.bautista16@gmail.com>
2026-03-31 15:14:34 +0200

    Add Owner search page with basic structure and styles

--- frontend/app/(owner)/search.tsx
+++ frontend/app/(owner)/search.tsx
@@ -1,9 +1,75 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
+import CustomButton from '../../src/components/CustomButton'
+import { router } from 'expo-router'
+import { colors, spacing, textSizes } from '../../src/constants/theme'
+import TimePicker from '../../src/components/TimePicker'
+import { useState } from 'react'
+import { dateToTimeString } from '../../src/utils/timeUtils'
+import DatePicker from '../../src/components/DatePicker'
+import { textStyles, layoutStyles } from '../../src/constants/theme'
 
 export default function Search() {
+    const [date, setDate] = useState(new Date())
+    const [startTime, setStartTime] = useState('09:00:00') //TODO: Import this from a commons file
+    const [endTime, setEndTime] = useState('18:00:00')
+
+
+
     return (
-        <View>
-            <Text>Search</Text>
+        <View style={layoutStyles.generalContainer}>
+            <CustomButton label="Back" onPressFn={() => router.back()} accesibilityLabel='Button for going back to home page' />
+            <View style={styles.contentContainer}>
+                <Text style={textStyles.displayHeader}>Drop-In Visits</Text>
+                <Text style={textStyles.paragraph}>When do you need a sitter?</Text>
+                <View style={styles.scheduleForm}>
+                    <View>
+                        <Text style={textStyles.sectionHeader}>Date</Text>
+                        <DatePicker value={date}
+                            onChange={(selectedDate) => setDate(selectedDate)} />
+                    </View>
+                    <View>
+                        <Text style={textStyles.sectionHeader}>Times</Text>
+                        <View style={styles.slotContainer}>
+                            <TimePicker
+                                value={startTime}
+                                onChange={(selectedDate) => setStartTime(dateToTimeString(selectedDate))}
+                            />
+                            <Text style={styles.separator}>-</Text>
+                            <TimePicker
+                                value={endTime}
+                                onChange={(selectedDate) => setEndTime(dateToTimeString(selectedDate))}
+                            />
+                        </View>
+                    </View>
+                </View>
+                <CustomButton label="Search" onPressFn={() => router.push({ pathname: '/(owner)/sitters', params: { date: date.toISOString().split('T')[0], start_time: startTime, end_time: endTime } })} accesibilityLabel='Button for searching sitters based on the provided parameters'></CustomButton>
+            </View>
         </View>
     )
 }
+
+
+//TODO: Extract repeated styles 
+const styles = StyleSheet.create({
+    contentContainer: {
+        flex: 1,
+        justifyContent: 'space-between',
+    },
+    scheduleForm: {
+        padding: spacing.md,
+        flex: 1,
+        gap: spacing.lg,
+
+    },
+    slotContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: spacing.md,
+        flex: 1,
+
+    },
+    separator: {
+        fontSize: 20,
+        color: colors.border,
+    },
+})
